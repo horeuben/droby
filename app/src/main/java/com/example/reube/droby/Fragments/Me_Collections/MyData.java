d3901e2b3bafe91f585d0792546af4441967ea30
WeirdVal <valerene_goh@mymail.sutd.edu.sg>
2017-07-14 16:55:01 +0800

    my fits commit

---
+++ app/src/main/java/com/example/reube/droby/Fragments/Me_Collections/MyData.java
@@ -0,0 +1,16 @@
+package com.example.reube.droby.Fragments.Me_Collections;
+
+import com.example.reube.droby.R;
+
+/**
+ * Created by ASUS on 7/5/2017.
+ */
+
+public class MyData {
+    static String[] nameArray = {"Formal", "Back to School", "Dance", "Everyday", "Dates"};
+    static String[] versionArray = {"Last Edit: 2 days ago", "Last Edit: 5 days ago", "Last Edit: 1 week ago", "Last Edit: 1 week ago", "Last Edit: 2 weeks ago"};
+
+    static Integer[] drawableArray = {R.drawable.outfit_1, R.drawable.outfit_2, R.drawable.outfit_3, R.drawable.outfit_4, R.drawable.outfit_5};
+
+    static Integer[] id_ = {0, 1, 2, 3, 4};
+}
