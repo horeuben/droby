b71824a60990e6d96ac5f5e51d1ee98bdd222f24
Reuben <reuben_ho@hotmail.com>
2017-07-25 18:57:47 +0800

    Commented away stuff that dont work

--- app/src/main/java/com/example/reube/droby/Fragments/Social/TrendingFragment.java
+++ app/src/main/java/com/example/reube/droby/Fragments/Social/TrendingFragment.java
@@ -6,12 +6,14 @@ import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Base64;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.reube.droby.Database.DatabaseHandler;
 import com.example.reube.droby.R;
@@ -65,18 +67,22 @@ public class TrendingFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_trending, container, false);
-        iv = (ImageView)getActivity().findViewById(R.id.imageView3);
+//        iv = (ImageView)getActivity().findViewById(R.id.imageView3);
 //        DatabaseHandler db = new DatabaseHandler(getContext());
 //        ArrayList<byte[]> test = db.syncTest();
-//        for(int i = 0;i<test.size();i++){
-//            byte [] imgBytes = test.get(i);
 //
-//            for (int l = 0; l<imgBytes.length;l++){
-//                Log.v(TAG,"HI IM HERE "+imgBytes[i]);
+//            byte [] imgBytes = test.get(0);
+//
+//            for (int i =0; i < imgBytes.length;i++){
+//                if (imgBytes[i]<0){
+//                    imgBytes[i]+=255;
+//                }Log.v("HERE IS WHERE WE ARE", "IMGBBYTES IS : "+imgBytes[i]);
+//
 //            }
 //            Bitmap bitmap = BitmapFactory.decodeByteArray(imgBytes, 0, imgBytes.length);
+//        Log.v("Tag","WE ARE HERE "+bitmap.getByteCount());
 //            iv.setImageBitmap(bitmap);
-//        }
+
 
         return view;
     }
